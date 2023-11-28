b9350c0b3a6cb6cc65ca04a2a9173571199baf47
ben11997 <abrahambenson90@gmail.com>
2023-11-28 11:13:24 +0100

    0x09. Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 0x09. Island perimeter """
+
+
+def island_perimeter(grid):
+    """ returns the perimetereter of the island described in grid """
+
+    if not grid:
+        return 0
+
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
